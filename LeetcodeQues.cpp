0d7b07dd508108b579438bbea3fca70044342e4a
Avinash Kumar Singh <your-email@example.com>
2026-04-10 23:39:27 +0530

    Day X - Problem Name

--- LeetcodeQues.cpp
+++ LeetcodeQues.cpp
@@ -320,3 +320,42 @@ public:
         return prefix;
     }
 };
+
+
+//Valid Parentheses
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            // If opening bracket → push
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            // If closing bracket
+            else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+
+                // Check matching
+                if ((ch == ')' && top == '(') ||
+                    (ch == '}' && top == '{') ||
+                    (ch == ']' && top == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        // If stack is empty → valid
+        return st.empty();
+    }
+};
