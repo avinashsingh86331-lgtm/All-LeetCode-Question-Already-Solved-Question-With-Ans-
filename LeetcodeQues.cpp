7a2ca8a6cc091b925e68928a4bb4ba5dfad3a2ef
Avinash Kumar Singh <your-email@example.com>
2026-04-16 23:26:24 +0530

    Day 11 - Search Insert Position

--- LeetcodeQues.cpp
+++ LeetcodeQues.cpp
@@ -540,3 +540,48 @@ int main() {
 
     return 0;
 }
+
+// Search Insert Position 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return left; // insertion position
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {1, 3, 5, 6};
+    int target;
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    int result = obj.searchInsert(nums, target);
+
+    cout << "Position: " << result << endl;
+
+    return 0;
+}
