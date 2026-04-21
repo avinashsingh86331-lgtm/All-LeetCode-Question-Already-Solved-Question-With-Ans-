f0f36e865fe948e34a686f9b7e569e614bae9941
Avinash Kumar Singh <your-email@example.com>
2026-04-21 19:48:07 +0530

    Day 14 - Integer to Roman

--- LeetcodeQues.cpp
+++ LeetcodeQues.cpp
@@ -688,3 +688,43 @@ int main() {
 
     return 0;
 }
+
+//Integer to Roman
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string intToRoman(int num) {
+    vector<int> values = {
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4, 1
+    };
+
+    vector<string> symbols = {
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV", "I"
+    };
+
+    string result = "";
+
+    for (int i = 0; i < values.size(); i++) {
+        while (num >= values[i]) {
+            result += symbols[i];
+            num -= values[i];
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    cout << "Roman numeral: " << intToRoman(num) << endl;
+
+    return 0;
+}
