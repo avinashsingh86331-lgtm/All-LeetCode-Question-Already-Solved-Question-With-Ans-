2721e6c3d97befe9a5551e35606b1f993b5cfeb6
Avinash Kumar Singh <your-email@example.com>
2026-04-26 23:01:13 +0530

    Day 19 - Rotate Image

--- LeetcodeQues.cpp
+++ LeetcodeQues.cpp
@@ -952,3 +952,54 @@ int main() {
 
     return 0;
 }
+
+//Rotate Image
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        // Step 1: Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter size of matrix (n): ";
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    Solution obj;
+    obj.rotate(matrix);
+
+    cout << "\nRotated Matrix (90 deg clockwise):\n";
+    for (auto &row : matrix) {
+        for (auto &val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
