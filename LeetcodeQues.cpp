e3a30a8f221e861b25a4e92cd47fe1128b02ec5f
Avinash Kumar Singh <your-email@example.com>
2026-04-30 16:55:23 +0530

    Day 21 - Best Time to Buy and Sell Stock

--- LeetcodeQues.cpp
+++ LeetcodeQues.cpp
@@ -1124,3 +1124,20 @@ int main() {
 }
 
 //Best Time to Buy and Sell Stock
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;   // Buy at lowest price
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice; // Sell for max profit
+            }
+        }
+
+        return maxProfit;
+    }
+};
