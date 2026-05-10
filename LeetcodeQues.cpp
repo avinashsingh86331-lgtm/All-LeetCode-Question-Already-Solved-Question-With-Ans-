94dadc3647bbdce14323564b95cf906af35a24ad
Avinash Kumar Singh <your-email@example.com>
2026-05-10 20:29:42 +0530

    Initial commit

--- LeetcodeQues.cpp
+++ LeetcodeQues.cpp
@@ -1291,3 +1291,52 @@ int main() {
 
     return 0;
 }
+
+//Valid Palindrome
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+bool isPalindrome(string s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+
+        // Skip non-alphanumeric characters
+        while (left < right && !isalnum(s[left])) {
+            left++;
+        }
+
+        while (left < right && !isalnum(s[right])) {
+            right--;
+        }
+
+        // Compare lowercase characters
+        if (tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main() {
+    string s;
+
+    cout << "Enter a string: ";
+    getline(cin, s);
+
+    if (isPalindrome(s)) {
+        cout << "Palindrome" << endl;
+    } else {
+        cout << "Not Palindrome" << endl;
+    }
+
+    return 0;
+}
