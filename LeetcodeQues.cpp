d1e7493e68d828466fe14ec498b07ec4d62257d7
Avinash Kumar Singh <your-email@example.com>
2026-06-12 15:30:20 +0530

    Day 31 - ount Square Sum Triples

--- LeetcodeQues.cpp
+++ LeetcodeQues.cpp
@@ -1437,3 +1437,27 @@ public:
         return check(root->left, root->right);
     }
 };
+
+
+// Count Square Sum Triples
+#include <cmath>
+
+class Solution {
+public:
+    int countTriples(int n) {
+        int count = 0;
+
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                int c2 = a * a + b * b;
+                int c = std::sqrt(c2);
+
+                if (c <= n && c * c == c2) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}; 
